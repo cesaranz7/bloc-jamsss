7861962e1cb92f9eaf5869d1d32c9941c74699d3
cesaranz7 <cesaranz@hotmail.com>
2017-01-16 22:01:02 -0500

    checkpoint 16 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,24 +1,27 @@
-var pointsArray = document.getElementsByClassName('point');
- 
- var animatePoints = function(points) {
+var animatePoints = function() {
     
-    var revealPoint = function (index) {
-        points[index].style.opacity = 1;
-         points[index].style.transform = "scaleX(1) translateY(0)";
-         points[index].style.msTransform = "scaleX(1) translateY(0)";
-         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
+    var revealPoint = function() {
+         
+        $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+    };
  
-  
-    for (var i = 0; i < points.length; i++) {
-        revealPoint(i);
-    }
+    $.each($('.point'), revealPoint);
 };
 
-window.onload = function() {
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+$(window).load(function() {
+    
+     if ($(window).height() > 950) {
+         animatePoints();
+     }
+    
+     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-    window.addEventListener('scroll', function(event) {
-        console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
-    })
-}
+   $(window).scroll(function(event) {
+        if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
+        }
+   });
+});
